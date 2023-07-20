c5dbdaf91c538ac92016fc8ec5e51b51f1749a39
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-07-20 20:47:49 +0900

    first commit

---
+++ src/utils/GlobalStyle.jsx
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    html,body {
+        padding : 0;
+        margin : 0;
+    }
+
+    a {
+        color : inherit;
+        text-decoration : none;
+    }
+
+    ul {
+        padding : 0;
+        margin : 0;
+        list-style : none;
+    }
+`;
+
+export default GlobalStyle;
