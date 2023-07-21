f0a6b70bab48042c37e7a89c43a110295b843ae6
TheON2 <sniperad@naver.com>
2023-07-21 17:59:36 +0900

    feat : add React-QueryApi / Axios-InterCeptor / reducers[news,user] / apply app react-query

---
+++ src/redux/reducers/newsSlice.js
@@ -0,0 +1,16 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const initialState = {
+  hasMoreNews:true,
+}
+
+const newsSlice = createSlice({
+  name: 'news',
+  initialState,
+  reducers: {
+  },
+})
+
+export const {} = newsSlice.actions
+
+export default newsSlice.reducer
