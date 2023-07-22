9700039254979a250b8bfcb98246aad675e3eb05
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-07-22 18:02:07 +0900

    fix : fix the newslist block

--- src/features/news/newslist/NewsList.jsx
+++ src/features/news/newslist/NewsList.jsx
@@ -20,7 +20,7 @@ export const NewsInfoBox = () => {
       <NewsInfoText size="1.8rem">
         <span>한미정상회담</span>의 검색 결과에요.
       </NewsInfoText>
-      <NewsInfoText size="1rem">🦔고슴이: 8개 찾았슴</NewsInfoText>
+      <NewsInfoText size="1.1rem">🦔고슴이: 8개 찾았슴</NewsInfoText>
       <NewsInfoButton>최신순</NewsInfoButton>
     </StNewsInfoBox>
   );
