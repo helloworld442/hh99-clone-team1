e7a7dc9e54b8c0be35aff420e09b0b75d50c5505
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-07-22 17:07:20 +0900

    feat : news info box ui / ux #11

--- src/features/news/newslist/style.js
+++ src/features/news/newslist/style.js
@@ -1,14 +1,47 @@
 import { styled } from "styled-components";
 
-export const StNavListBox = styled.div`
+export const NewsListCol = styled.div`
   width: 100vw;
   padding: 0 5%;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
 `;
 
-export const StNavList = styled.ul`
+export const StNewsList = styled.ul`
   width: 100%;
   border-left: 1px solid #333;
   display: flex;
   flex-wrap: wrap;
+`;
+
+export const StNewsInfoBox = styled.div`
+  width: 100%;
+  height: 200px;
+  margin-top: 30px;
+  border-bottom: 1px solid #333;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const NewsInfoText = styled.span`
+  margin-top: 12px;
+  font-size: ${(props) => props.size};
+  font-weight: 500;
+  span {
+    color: rgb(153, 153, 153);
+  }
+`;
+
+export const NewsInfoButton = styled.button`
+  width: 80px;
+  padding: 0.4rem 0;
+  margin-top: 35px;
+  border: 2px solid #000;
+  outline: none;
+  font-size: 0.9rem;
+  font-weight: 600;
+  color: #fff;
+  background: #000;
 `;
