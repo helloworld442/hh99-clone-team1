4f3be97034ae6b05a4cf4dd154936bbb5c06f060
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-07-24 12:49:43 +0900

    fix : fix the news detail layout header

--- src/pages/news/Detail.jsx
+++ src/pages/news/Detail.jsx
@@ -1,7 +1,17 @@
+import NewsHeader from "../../features/common/Header/Header";
+import NewsHeaderNav from "../../features/common/Header/HeaderNav";
+import Layout from "../../features/common/Layout";
 import NewsDetail from "../../features/news/newsdetail/NewsDetail";
 
 const Detail = () => {
-  return <NewsDetail />;
+  return (
+    <Layout>
+      <NewsHeader>
+        <NewsHeaderNav />
+      </NewsHeader>
+      <NewsDetail />
+    </Layout>
+  );
 };
 
 export default Detail;
