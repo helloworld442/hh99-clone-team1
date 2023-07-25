f3312a2610a7b3b6d7b03d9971c6749f2c754d7d
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-07-26 02:33:58 +0900

    fix : fix the news detail page

--- src/redux/reducers/newsSlice.js
+++ src/redux/reducers/newsSlice.js
@@ -8,18 +8,9 @@ const initialState = {
 const newsSlice = createSlice({
   name: "news",
   initialState,
-  reducers: {
-    CHANGE_INPUT: (state, action) => {
-      return (state.searchInput = action.payload);
-    },
-    GET_INPUT_BY_NAME: (state, action) => {
-      return (state.news = action.payload.news.find(
-        (news) => news.title === action.payload.name
-      ));
-    },
-  },
+  reducers: {},
 });
 
-export const { CHANGE_INPUT, GET_INPUT_BY_NAME } = newsSlice.actions;
+export const {} = newsSlice.actions;
 
 export default newsSlice.reducer;
