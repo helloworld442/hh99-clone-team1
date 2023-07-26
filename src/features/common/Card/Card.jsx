6832e183729a48905232350db7d010443eb4acae
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-07-26 16:11:33 +0900

    hotfix : 게시글 내용 수정

--- src/features/common/Card/Card.jsx
+++ src/features/common/Card/Card.jsx
@@ -25,7 +25,7 @@ const StCardImg = styled.img`
 `;
 
 const StCardText = styled.span`
-  width: 100%;
+  width: 300px;
   margin: 8px 24px;
   font-size: ${(props) => props.size};
   font-weight: ${(props) => props.weight};
